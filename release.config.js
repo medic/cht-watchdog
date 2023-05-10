2295ff14e0de71c51b9d74035b48f3a7b13095a2
m5r <mokht@rmi.al>
2023-05-10 04:41:20 +0200

    feat(#34): bump version in package.json with `@semantic-release/npm`

--- release.config.js
+++ release.config.js
@@ -3,18 +3,24 @@ module.exports = {
   'plugins': [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false
+      },
+    ],
     '@semantic-release/github',
     [
       '@semantic-release/changelog',
       {
-        'changelogFile': 'CHANGELOG.md',
+        changelogFile: 'CHANGELOG.md',
       },
     ],
     [
       '@semantic-release/git',
       {
-        'assets': ['package.json', 'package-lock.json', 'CHANGELOG.md'],
-        'message': 'chore(release): ${nextRelease.version}',
+        assets: ['package.json', 'package-lock.json', 'CHANGELOG.md'],
+        message: 'chore(release): ${nextRelease.version}',
       },
     ],
   ],
