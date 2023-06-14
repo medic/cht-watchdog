0bcdd552edd5b86bed4236340c4606523c27cd24
Joshua Kuestersteffen <jkuester@kuester7.com>
2023-06-14 14:25:05 -0500

    feat(#52): set dynamic threshold for Outbound Push Backlog (#62)
    Co-authored-by: Tatiana Lépiz <94494491+tatilepizs@users.noreply.github.com>

--- development/tests/integration/alerting/outbound-push-backlog.spec.js
+++ development/tests/integration/alerting/outbound-push-backlog.spec.js
@@ -2,22 +2,22 @@ const prometheus = require('../utils/prometheus');
 const grafana = require('../utils/grafana');
 const { expect } = require('chai');
 
-const ALERT_RULE_NAME = 'Sentinel Backlog';
+const ALERT_RULE_NAME = 'Outbound Push Backlog';
 
-describe('Sentinel Backlog alert rule', () => {
+describe('Outbound Push Backlog alert rule', () => {
   [
-    [499, 10, 'Normal'],
-    [520, 10, 'Pending'],
-    [620, 121, 'Normal'],
-    [620, 100, 'Pending']
+    [9, 100, 'Normal'],
+    [10, 100, 'Pending'],
+    [54, 1000, 'Normal'],
+    [55, 1000, 'Pending']
   ].forEach(([backlogHr, updateSeqHr, state]) => {
-    it(`is [${state}] with sentinel backlog rate [${backlogHr}] and DB change rate [${updateSeqHr}]`, async () => {
+    it(`is [${state}] with outbound push backlog rate [${backlogHr}] and DB change rate [${updateSeqHr}]`, async () => {
       const updateSeq = updateSeqHr * 720; // 720 hours in 30 days
       const endMs = Date.now();
       const startMs = endMs - (1000 * 60 * 60); // 1 hour ago
 
-      const startBacklogCount = prometheus.createMetric('cht_sentinel_backlog_count', 0, startMs);
-      const endBacklogCount = prometheus.createMetric('cht_sentinel_backlog_count', backlogHr, endMs);
+      const startBacklogCount = prometheus.createMetric('cht_outbound_push_backlog_count', 0, startMs);
+      const endBacklogCount = prometheus.createMetric('cht_outbound_push_backlog_count', backlogHr, endMs);
       const backlogMetrics = prometheus.extrapolateMetrics(startBacklogCount, endBacklogCount);
 
       const startUpdateSeq = prometheus.createMetric('cht_couchdb_update_sequence', 0, startMs, { db: 'medic' });
