5986d3d01bb41529374c96a2f342a6670c498828
mrjones-plip <mrjones-plip@plip.com>
2024-04-23 12:51:25 -0700

    add dev restart script, update detail dashboard json

---
+++ development/kill.start.ips.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+echo '
+    ,    /-.
+   ((___/ __>
+   /      }      ---- CHT WATCHDOG RESTART SCRIPT ----
+   \ .--.(
+    \\   \\
+'
+
+echo;echo "Killing...";echo
+docker compose  \
+    -f docker-compose.yml \
+    -f exporters/postgres/compose.yml \
+    kill
+
+echo;echo "Starting...";echo
+docker compose  \
+    -f docker-compose.yml \
+    -f exporters/postgres/compose.yml \
+    up -d \
+    --remove-orphans
+
+# shellcheck disable=SC2046
+ips=$(docker inspect $(docker ps -q ) \
+  --format='{{ printf "%-50s" .Name}} {{range .NetworkSettings.Networks}}{{.IPAddress}} {{end}}'  \
+  | tr "\/" " " | tr -s " " )
+
+# shellcheck disable=SC2046
+readarray -t ipsArray <<< $(echo -e "${ips}")
+
+final=""
+# loop it
+for ipLine in "${ipsArray[@]}"; do
+  ip=$(echo "$ipLine"  | cut -f3 -d" ")
+  name=$(echo "$ipLine"  |cut -f2 -d" ")
+  case $name in
+    cht-watchdog-extra_sql_exporter-1)
+      portPath=":9187/metrics";;
+    cht-watchdog-grafana-1)
+      portPath=":3000";;
+    cht-watchdog-postgres-exporter-1)
+      portPath=":9187/metrics";;
+    cht-watchdog-json-exporter-1)
+      portPath=":7979/metrics";;
+    cht-watchdog-prometheus-1) 
+      portPath=":9090/targets?search=";;
+    cht-watchdog-sql_exporter-1)
+      portPath=":9399/metrics";;
+    *)
+      portPath="";;
+  esac
+  final+="${name}  http://${ip}${portPath}\n"
+done
+
+echo;echo "Services:";echo
+echo -e "${final}"  | column -t
+echo
